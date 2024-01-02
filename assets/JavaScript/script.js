bcbb67a66dad2ca7af894c0b120db377cc4fcd5a
iTPr0 <66981750+iTPr0@users.noreply.github.com>
2024-01-02 11:13:50 -0300

    up

---
+++ assets/JavaScript/script.js
@@ -0,0 +1,189 @@
+$(function () {
+    $("#file-manager").dxFileManager({
+        name: "fileManager",
+        fileSystemProvider: customProvider,
+        currentPath: "Documentos",
+        rootFolderName: "Root",
+        permissions: {
+            download: true
+        },
+        language: "pt-BR",
+        onToolbarItemClick(e) {
+            if (e.itemData === "delete") {
+                var itemName = e.component.getSelectedItems()[0].name;
+                setTimeout(function () {
+                    $("div.dx-filemanager-dialog-delete-item > div").attr("title", itemName);
+                }, 0);
+            }
+        },
+        contextMenu: {
+            items: ["create", {
+                name: "download"
+            }]
+        },
+        customizeDetailColumns: (columns) => {
+            columns.push({
+                caption: "Criado",
+                dataField: "dataItem.creator"
+            });
+            return columns;
+        }
+    });
+});
+
+var fileSystem = [{
+    name: "Sistemas de Arquivos",
+    isDirectory: true,
+    items: [{
+        name: "Antivirus",
+        isDirectory: true,
+        creator: "Aeon",
+        items: [{
+            name: "PANDAFREEAV",
+            isDirectory: false,
+            creator: "Aeon",
+            size: 1024,
+            download: "https://github.com/iTPr0/FileExplorer/files/13810769/PANDAFREEAV.zip"
+        },]
+    }, {
+        name: "Drivers & Ativação",
+        isDirectory: true,
+        creator: "Aeon",
+        items: [{
+            name: "Script",
+            isDirectory: false,
+            creator: "Aeon",
+            size: 1024,
+            download: "https://github.com/iTPr0/FileExplorer/files/13831946/script.zip"
+        }]
+    }, {
+        name: "OfficeSetup",
+        isDirectory: true,
+        creator: "Aeon",
+        items: [{
+            name: "OfficeSetup",
+            isDirectory: false,
+            creator: "Aeon",
+            size: 1024,
+            download: "https://github.com/iTPr0/FileExplorer/files/13810797/OfficeSetup.zip"
+        }]
+    }, {
+        name: "Visual-C-Runtimes",
+        isDirectory: true,
+        creator: "Aeon",
+        items: [{
+            name: "VcRedist.ALL",
+            isDirectory: false,
+            creator: "Aeon",
+            size: 1024,
+            download: "https://github.com/iTPr0/FileExplorer/files/13841366/VcRedist.ALL.zip"
+        }]
+    }, {
+        name: "Sistemas Operacionais",
+        isDirectory: true,
+        creator: "Aeon",
+        items: [{
+            name: "Win10",
+            isDirectory: true,
+            creator: "Aeon",
+            items: [{
+                name: "Win.32&64bits.BrazilianPortuguese",
+                isDirectory: false,
+                creator: "Aeon",
+                size: 20480,
+                download: "https://github.com/iTPr0/FileExplorer/files/13840867/Win10.zip"
+            }]
+        }, {
+            name: "Win11",
+            isDirectory: true,
+            creator: "Aeon",
+            items: [{
+                name: "Win.64Bits.BrazilianPortuguese",
+                isDirectory: false,
+                creator: "Aeon",
+                size: 20480,
+                download: "https://github.com/iTPr0/FileExplorer/files/13840875/Win11.zip"
+            }, {
+                name: "PorqueNaoWin11",
+                isDirectory: true,
+                creator: "Aeon",
+                items: [{
+                    name: "WhyNowWin11",
+                    isDirectory: false,
+                    creator: "Aeon",
+                    size: 20480,
+                    download: "https://github.com/iTPr0/FileExplorer/files/13840909/Por.que.Nao.Win11.zip"
+                }]
+            },]
+        }, {
+            name: "WinAIO",
+            isDirectory: true,
+            creator: "Aeon",
+            items: [{
+                name: "Win.AIO.32&64bits.BrazilianPortuguese",
+                isDirectory: false,
+                creator: "Aeon",
+                size: 20480,
+                download: "https://github.com/iTPr0/FileExplorer/files/13840886/WinAIO.zip"
+            }]
+        }]
+    },]
+}, {
+    name: "Desktop",
+    isDirectory: true,
+    creator: "Aeon",
+    items: [{
+        name: "Downloads",
+        isDirectory: true,
+        creator: "Aeon",
+        items: [{
+            name: "DriverEasy",
+            isDirectory: false,
+            creator: "Aeon",
+            size: 20480,
+            download: "https://github.com/iTPr0/FileExplorer/files/13810800/DriverEasy.zip"
+        },]
+    }]
+}];
+
+var objectProvider = new DevExpress.fileManagement.ObjectFileSystemProvider({
+    data: fileSystem
+});
+
+var customProvider = new DevExpress.fileManagement.CustomFileSystemProvider({
+    getItems: function (pathInfo) {
+        return objectProvider.getItems(pathInfo);
+    },
+
+    downloadItems: function (items) {
+        if (items.length === 1) {
+            downloadSingleFile(items[0]);
+        } else {
+            downloadMultipleFiles(items);
+        }
+    }
+});
+
+function downloadSingleFile(file) {
+    var downloadLink = file.dataItem.dataItem.download || null;
+
+    if (downloadLink) {
+        window.open(downloadLink, '_blank');
+    } else {
+        console.error("Link de download não disponível para o arquivo selecionado.");
+    }
+}
+
+function downloadMultipleFiles(files) {
+    var zip = new JSZip();
+
+    files.forEach(function (file) {
+        var content = file.dataItem.dataItem.download || "";
+        zip.file(file.name, content, { base64: true });
+    });
+
+    zip.generateAsync({ type: "blob" })
+        .then(function (content) {
+            saveAs(content, "Download.zip");
+        });
+}
---
+++ readme.md
@@ -0,0 +1,61 @@
+# <p align="center">File Explorer 📁💻</p>
+
+### <p align="center">Uma ferramenta poderosa para técnicos de informática! 🚀 </br>Este explorador de arquivos é um kit completo contendo programas essenciais, sistemas operacionais e muito mais.</p>
+
+### Estrutura de Pastas 🗂️
+
+1. 🦠 Antivirus
+2. 🚗 Drivers
+3. 🏢 OfficeSetup
+4. 💻 Sistemas Operacionais
+
+### Contribuindo :handshake:
+
+Esperamos que este File Explorer seja uma ferramenta valiosa para suas necessidades técnicas! Sinta-se à vontade para contribuir para este projeto abrindo problemas ou enviando solicitações de recebimento. Se você encontrar algum problema ou tiver sugestões de melhorias, por favor faça um fork do repositório e suba uma solicitação de pull com suas alterações.
+
+### ❤️ Apoio, suporte.
+
+Se por acaso você adora este projeto, deixe uma estrela no repo.
+
+<div align="center">
+ <img src="https://github-production-user-asset-6210df.s3.amazonaws.com/66981750/262346028-b6bf186e-5554-4736-a192-956402c5b0db.jpg" width="15%" height="15%">
+<br/>
+
+[![WebSite](https://img.shields.io/badge/website-000000?style=for-the-badge&logo=About.me&logoColor=white)](https://links.tifodao.com)
+
+</div>
+
+<div align="center">
+Desenvolvido com ❤️ no Brasil 🌟 <br/>
+
+[![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black)](https://www.javascript.com/)
+
+</div>
+
+### Licença :page_facing_up:
+
+Este projeto é licenciado sob a Licença MIT.
+
+```
+MIT License
+
+Copyright (c) [2024] [Diego Melo]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
